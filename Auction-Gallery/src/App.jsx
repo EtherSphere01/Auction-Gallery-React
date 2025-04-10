3c66e1df6a68a73d4379520fd32b0e17d4ac6eba
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-11 01:43:45 +0600

    header added

--- Auction-Gallery/src/App.jsx
+++ Auction-Gallery/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
+import Items from "./Items/Items";
 
 function App() {
-
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+
+      {/* main section */}
+
+      <div>
+        {/* items section */}
+        <div>
+          <Items></Items>
+        </div>
+
+        {/* Favorite items */}
+        <div></div>
+      </div>
     </>
   );
 }
---
+++ Auction-Gallery/src/Items/Items.jsx
@@ -0,0 +1,52 @@
+import React, { use, useEffect, useState } from "react";
+
+const Items = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const data = "resources.json";
+    fetch(data)
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+  console.log(items);
+  return (
+    <div className="container mx-auto mt-20">
+      <h1 className="text-2xl font-bold text-[#0E2954]">Activate Auctions</h1>
+      <p className="pb-7">Discover and bid on extraordinary items</p>
+
+      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        <table className="table">
+          {/* head */}
+          <thead className="text-black">
+            <tr>
+              <th>Items</th>
+              <th>Current Bid</th>
+              <th>Time Left</th>
+              <th>Bid Now</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td className="flex items-center gap-3 ">
+                  <img src={item.image} alt="" className="w-16 h-16" />
+                  <div>
+                    <p className="text-sm text-gray-500">{item.title}</p>
+                  </div>
+                </td>
+                <td>$ {item.currentBidPrice}</td>
+                <td>{item.timeLeft}</td>
+                <td>
+                  <button className="btn btn-primary">Bid Now</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Items;
