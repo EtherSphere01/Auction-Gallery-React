1e73aebd092a4470ce8a566285f94e181985f173
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-11 16:11:07 +0600

    header added

--- Auction-Gallery/src/Hero/Hero.jsx
+++ Auction-Gallery/src/Hero/Hero.jsx
@@ -4,24 +4,26 @@ const Hero = () => {
   return (
     <div className="">
       <div
-        className="hero min-h-[75vh]  "
+        className="hero min-h-[75vh]"
         style={{
           backgroundImage: "url(../../pictures/Banner-min.jpg)",
         }}
       >
         <div className="hero-overlay"></div>
-        <div className="hero-content text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">
-              Bid on Unique Items from Around the World
-            </h1>
-            <p className="mb-5">
-              Discover rare collectibles, luxury goods, and vintage treasures in
-              our curated auctions
-            </p>
-            <button className="btn btn-primary rounded-4xl bg-white text-black shadow-none border-black">
-              Explore Auctions
-            </button>
+        <div className="container mx-auto flex">
+          <div className="hero-content text-neutral-content ">
+            <div className="md:max-w-md">
+              <h1 className="mb-5 md:text-5xl text-4xl font-bold">
+                Bid on Unique Items from Around the World
+              </h1>
+              <p className="mb-5">
+                Discover rare collectibles, luxury goods, and vintage treasures
+                in our curated auctions
+              </p>
+              <button className="btn btn-primary rounded-4xl bg-white text-black shadow-none border-black">
+                Explore Auctions
+              </button>
+            </div>
           </div>
         </div>
       </div>
