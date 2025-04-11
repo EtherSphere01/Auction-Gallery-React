9353a6507eb382456bff120c979b59b7b38bbc89
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-11 15:57:20 +0600

    header added

--- Auction-Gallery/src/Hero/Hero.jsx
+++ Auction-Gallery/src/Hero/Hero.jsx
@@ -2,26 +2,28 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative">
-      <div className="md:h-150">
-        <img
-          src="../../pictures/Banner-min.jpg"
-          alt=""
-          className="w-full h-full object-cover "
-        />
-      </div>
-
-      <div className="container mx-auto absolute md:top-[35%] md:left-[10%] flex flex-col md:gap-5 top-[20%] left-5 gap-2">
-        <p className="text-white md:text-3xl text-xl font-bold flex flex-wrap">
-          Bid on Unique items from<br></br> Around the World
-        </p>
-        <p className="text-white">
-          Discover rare collectibles, luxury goods, and vintage <br></br>
-          treasures in our curated auctions
-        </p>
-        <button className="btn btn-primary border-black shadow-none w-40 bg-white text-black rounded-4xl py-1.5">
-          Explore Auctions
-        </button>
+    <div className="">
+      <div
+        className="hero min-h-[75vh]  "
+        style={{
+          backgroundImage: "url(../../pictures/Banner-min.jpg)",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">
+              Bid on Unique Items from Around the World
+            </h1>
+            <p className="mb-5">
+              Discover rare collectibles, luxury goods, and vintage treasures in
+              our curated auctions
+            </p>
+            <button className="btn btn-primary rounded-4xl bg-white text-black shadow-none border-black">
+              Explore Auctions
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
